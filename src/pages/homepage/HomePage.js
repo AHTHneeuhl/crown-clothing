c8d66dba069b070f10724354862e43fc52f23cb7
Shubham Patrick <ahthneeuhl@gmail.com>
2022-04-05 00:59:40 +0530

    added project architecture and created Directory and MenuItem component files

---
+++ src/pages/homepage/HomePage.js
@@ -0,0 +1,12 @@
+import Directory from "../../components/directory/Directory";
+import "./HomePage.styles.scss";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
