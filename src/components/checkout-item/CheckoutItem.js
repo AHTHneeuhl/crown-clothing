24a024ef9d0a2a70f4119e6fe58298c0b3051c42
Shubham Patrick <ahthneeuhl@gmail.com>
2022-04-10 12:45:28 +0530

    Added clearItem and removeItem functionality to CheckoutItem component

--- src/components/checkout-item/CheckoutItem.js
+++ src/components/checkout-item/CheckoutItem.js
@@ -1,15 +1,37 @@
+import { connect } from "react-redux";
+import { addItem, clearItem, removeItem } from "../../redux/cart/cartActions";
 import "./CheckoutItem.styles.scss";
 
-const CheckoutItem = ({ cartItem: { imageUrl, name, quantity, price } }) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img src={imageUrl} alt="item" />
-    </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-    <div className="remove-button">&#10005;</div>
-  </div>
-);
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
+  const { imageUrl, name, quantity, price } = cartItem;
 
-export default CheckoutItem;
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>
+          &#10094;
+        </div>
+        <span className="value"> {quantity} </span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
+      </span>
+      <span className="price">{price}</span>
+      <div className="remove-button" onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </div>
+    </div>
+  );
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItem(item)),
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(removeItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/redux/cart/cartActions.js
+++ src/redux/cart/cartActions.js
@@ -1,6 +1,8 @@
 export const CartActionTypes = {
   TOGGLE_CART: "TOGGLE_CART",
   ADD_ITEM: "ADD_ITEM",
+  REMOVE_ITEM: "REMOVE_ITEM",
+  CLEAR_ITEM_FROM_CART: "CLEAR_ITEM_FROM_CART",
 };
 
 export const toggleCart = () => ({
@@ -9,5 +11,15 @@ export const toggleCart = () => ({
 
 export const addItem = (item) => ({
   type: CartActionTypes.ADD_ITEM,
+  payload: item,
+});
+
+export const clearItem = (item) => ({
+  type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item,
+});
+
+export const removeItem = (item) => ({
+  type: CartActionTypes.REMOVE_ITEM,
   payload: item,
 });
